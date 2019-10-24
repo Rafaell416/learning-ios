7db15e65a712fe4333340f8ad53f494204cdeb8a
Rafael Villarreal <rvillarreal416@gmail.com>
2019-10-24 14:02:58 -0500

    initial files

---
+++ AppTwo/AppTwo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AppTwo
+//
+//  Created by Rafael Villarreal on 10/19/19.
+//  Copyright © 2019 Rafael Villarreal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Login Screen/Login Screen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login Screen
+//
+//  Created by Rafael Villarreal on 10/17/19.
+//  Copyright © 2019 Rafael Villarreal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
